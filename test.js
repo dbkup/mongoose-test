b2e72e19be8efba8a8a41d336a37793d77265830
dbkup <boris.kupusovic@gmail.com>
2015-12-28 17:01:21 +0100

    initial commit

---
+++ test.js
@@ -0,0 +1,33 @@
+var Util = require('util'),
+    Async = require('async'),
+    Mongoose = require('mongoose'),
+    Schema = Mongoose.Schema,
+    MongooseDbInstance = Mongoose.createConnection('mongodb://localhost:27017/playground');
+
+var Person = MongooseDbInstance.model('Person', Schema({
+    name: String,
+    age: Number
+}), 'persons');
+
+var persons = [
+    {name: 'Meki', age: 29},
+    {name: 'Friend', age: 27},
+    {name: 'Tipper', age: 119},
+    {name: 'Blabber', age: 319},
+    {name: 'Trickster', age: 119}
+];
+
+Async.each(persons, function (person, callback) {
+    Person.create(person, callback);
+}, function (err) {
+    if (err) {
+        return console.log(err);
+    }
+
+    var agg = Person.aggregate({$match: {age: {$gt: 30}}});
+    agg.append([{$match: {name: /^t/i}}, {$match: {name: /ster$/i}}]);
+
+    agg.exec(function (e, d) {
+        console.log(Util.inspect(e || d, {depth: 10}));
+    });
+});
